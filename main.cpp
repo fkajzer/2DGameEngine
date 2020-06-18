1dea9362a15b7b6833ee939ddd9a64d78f98c548
fkajzer <filipkajzer@gmail.com>
2020-06-18 22:51:31 +0200

    limit frames  to 60 per second

--- main.cpp
+++ main.cpp
@@ -13,14 +13,23 @@ int main(int argc, const char *argv[]) {
     game = new Game();
     game->init("2DGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
 
-    while (game->running()) {
-        
-
+    while (game->running())
+    {
+        frameStart = SDL_GetTicks();
 
         game->handleEvents();
         game->update();
         game->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+    
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
     }
+
+
 
     game->clean();
 
